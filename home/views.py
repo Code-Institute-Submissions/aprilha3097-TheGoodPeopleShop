94427309d3f72b025bd335f24f6cccec97a1d28f
aprilha3097 <aprilha3097@gmail.com>
2021-02-21 20:09:16 +0000

    Added url, view and template for the about page that contains in detail information of the sites purpose and creators

--- home/views.py
+++ home/views.py
@@ -6,3 +6,8 @@ def index(request):
     """ A view to return the Index page """
 
     return render(request, 'home/index.html')
+
+def about(request):
+    """ A view to return the About page """
+
+    return render(request, 'home/about.html')
