b1957afdca307e01616ef14fde160575d9f09110
aprilha3097 <aprilha3097@gmail.com>
2021-02-22 12:35:18 +0000

    Added funcationality for product detail so user is able to click on an product and be taken to a seperate view for individual product details

--- products/views.py
+++ products/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Product
 
 def all_products(request):
+    """ View to view all products """
     products = Product.objects.all()
     return render(request, 'products/products.html', {'all_items': products})
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
