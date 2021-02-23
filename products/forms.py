7756a0cd316c7a32dc47f5ac471e756dfefd143e
aprilha3097 <aprilha3097@gmail.com>
2021-02-23 21:29:38 +0000

    Added link so authenticated user can add a new product

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,5 @@
-from django import forms 
+from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Product
 
 class ProductForm(forms.ModelForm):
@@ -6,3 +7,11 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
--- products/models.py
+++ products/models.py
@@ -79,7 +79,7 @@ class Product(models.Model):
         default = '',
     )
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=False, blank=False)
 
     def __str__(self):
         return self.name
