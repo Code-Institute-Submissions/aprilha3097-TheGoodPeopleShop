22a99a8651ba78400dd29b469199709db53260ef
aprilha3097 <aprilha3097@gmail.com>
2021-02-23 10:48:37 +0000

    Added to search funcationality for quanitity of available products AND added sort selector for user in future to filter through products, no functioanlity yet

--- products/views.py
+++ products/views.py
@@ -7,21 +7,40 @@ def all_products(request):
     """ View to view all products """
     products = Product.objects.all()
     query = None
-    
-    if 'q' in request.GET:
-        query = request.GET['q']
-        if not query:
-            messages.error(request, "you did not enter any search criteria")
-            return redirect(reverse('products'))
+    sort = None
+    direction = None
 
-        queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(charity__icontains=query)
-        products = products.filter(queries)
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(charity__icontains=query)
+            products = products.filter(queries)
+
+    current_sorting  = f'{sort}_{direction}'
 
     context = {
         'products': products,
+        'search_term': query,
+        'current_sorting': current_sorting,
     }
 
-    return render(request, 'products/products.html', {'all_items': products})
+    return render(request, 'products/products.html', context)
 
 def product_detail(request, product_id):
     """ A view to show individual product details """
