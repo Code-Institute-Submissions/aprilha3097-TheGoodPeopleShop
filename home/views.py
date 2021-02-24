cde0c67afdabc57cdfec0896d11116452fb7d8c2
aprilha3097 <aprilha3097@gmail.com>
2021-02-24 14:39:10 +0000

    Removed section below hero image on home page

--- home/views.py
+++ home/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from products.models import Product
 
 def index(request):
     """ A view to return the Index page """
 
     return render(request, 'home/index.html')
+
 
 def about(request):
     """ A view to return the About page """
