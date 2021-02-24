4ee260c82d3d48e37c14f62020af3df3bf3b5519
aprilha3097 <aprilha3097@gmail.com>
2021-02-24 22:29:37 +0000

    Added view, url and template for user order history

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+
+from checkout.models import Order
+
 
 def profile(request):
+    """ Display the user's profile """
+
     template = 'profiles/profile.html'
 
     return render(request, template)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, template, context)
