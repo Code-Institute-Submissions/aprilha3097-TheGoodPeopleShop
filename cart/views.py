d66d279e8bd0a8375fe3222589478a7e6263dab9
aprilha3097 <aprilha3097@gmail.com>
2021-02-24 17:53:53 +0000

    Added template url and view for add to cart functionality

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+
+from products.models import Product
 
 # Create your views here.
 
@@ -7,3 +9,14 @@ def view_cart(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'cart/cart.html')
+
+def add_to_cart(request, item_id):
+    product = get_object_or_404(Product, pk=item_id)
+
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] = cart
+    
+    request.session['cart'] = cart
+    return redirect(redirect_url)
