58355fdc2f4b2d1ff3711fdd29ad1ebab8ddfc8f
aprilha3097 <aprilha3097@gmail.com>
2021-02-24 23:34:41 +0000

    BUG superuser is unable to login with error RelatedObjectDoesNotExist, but normal registered users are able to login

--- profiles/forms.py
+++ profiles/forms.py
@@ -13,7 +13,16 @@ class UserProfileForm(forms.ModelForm):
         labels and set autofocus on first field
         """
         super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'Phone Number',
+            'default_postcode': 'Postal Code',
+            'default_town_or_city': 'Town or City',
+            'default_street_address1': 'Street Address 1',
+            'default_street_address2': 'Street Address 2',
+            'default_county': 'County, State or Locality',
+        }
 
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
         for field in self.fields:
             placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
