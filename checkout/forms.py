5e7bc4a2b36e472a70250a74b2bf4a1c4127a6b9
aprilha3097 <aprilha3097@gmail.com>
2021-02-25 14:22:56 +0000

    Amended admin and forms for checkout page, specified fields will be required when checking out

--- checkout/forms.py
+++ checkout/forms.py
@@ -7,7 +7,7 @@ class OrderForm(forms.ModelForm):
         model = Order
         fields = ('full_name', 'email', 'phone_number',
                   'street_address1', 'street_address2',
-                  'town_or_city', 'postcode',
+                  'town_or_city', 'postcode', 'country',
                   'county',)
 
     def __init__(self, *args, **kwargs):
@@ -20,6 +20,7 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
+            'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
@@ -29,7 +30,10 @@ class OrderForm(forms.ModelForm):
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            placeholder = placeholders[field]
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
