82d83f88f880570559f60e4f3cdee0f978b29397
aprilha3097 <aprilha3097@gmail.com>
2021-02-26 00:27:50 +0000

    User is able to view their order history

--- profiles/views.py
+++ profiles/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render, get_object_or_404
-from django.contrib import messages 
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
 
 from checkout.models import Order
 
-# Create your views here.
 
 @login_required
 def profile(request):
-    """ Display the user's profile """ 
+    """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
@@ -28,7 +27,7 @@ def profile(request):
     context = {
         'form': form,
         'orders': orders,
-        'on_profile_page': True,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
@@ -37,15 +36,15 @@ def profile(request):
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
-    messsages.info(request, (
-        f'This is a past confirmation for order number {order_number}.'
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
         'A confirmation email was sent on the order date.'
     ))
 
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
-        'form_profile': True,
+        'from_profile': True,
     }
 
     return render(request, template, context)
